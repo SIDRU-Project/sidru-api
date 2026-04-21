163228556f9fff608c4723b9c3c3ea7e7cde949c
AdrianoSCruzP <u202210697@upc.edu.pe>
2026-04-21 09:12:00 -0500

    chore: US-01 scaffold Spring Boot 3.4.5 (Java 21, Maven, PostgreSQL, actuator health)

---
+++ src/main/java/com/sidru/sidru_api/SidruApiApplication.java
@@ -0,0 +1,16 @@
+package com.sidru.sidru_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableScheduling
+public class SidruApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SidruApiApplication.class, args);
+    }
+}
